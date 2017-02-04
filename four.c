499d7d5275423880706bf89657dc52b1f8a79f60
Live session user <gautamee.naik11@gmail.com>
2017-02-04 19:42:38 +0000

    ifixes issue with thread execution

--- four.c
+++ four.c
@@ -3,17 +3,24 @@
 #include<pthread.h>
 
 typedef enum { false, true  } bool;
-
+//thread to act as a microcontroller that generates data every 30 seconds
 void *pseudo_controller(void *vargp)
 {
-printf("hello im controller");
+printf("hello im pseudo controller\n");
 }
-main()
+
+//thread to act as a data server that connects to the microcontroller and receives data from the same
+void *data_server(void *dservp){
+printf("Server to receive data from microcontroller");
+}
+
+int main()
 {
 pthread_t tid;
-
+printf("Welcome to message generation system for microcontroller\n");
 //create  a  thread for running the  pseudo controller 
-pthread_create(&tid, NULL, pseudo_controller, (void *)0);
-return 0;
+pthread_create(&tid, NULL, pseudo_controller, NULL);
+pthread_join(tid, NULL);
+//return 0;
 }
 
