73bfad046381842a2b43102a47e8b9817fa1b462
gautamee29 <gautamee.naik11@gmail.com>
2017-02-06 18:32:49 +0530

    Update level3.c

--- level3.c
+++ level3.c
@@ -2,30 +2,53 @@
 #include<stdlib.h>
 #include<pthread.h>
 #include<unistd.h>
-typedef enum { false, true  } bool;
+#include<unistd.h>
+#define TRUE 1
+#define FALSE 0
+#define  BUFFER_SIZE  6000000
+char connect = TRUE;
+char mc_buffer[BUFFER_SIZE] = "$GPGSV,3,1,11,09,76,148,32,05,55,242,29,17,33,054,30,14,27,314,24*71";
 //thread to act as a microcontroller that generates data every 30 seconds
 void *pseudo_controller(void *vargp)
 {
-printf("hello im pseudo controller\n");
   //run a function every 30 seconds
     while(1)
-    {         
-printf("$GPGSV,3,1,11,09,76,148,32,05,55,242,29,17,33,054,30,14,27,314,24*71\n");
-fflush(stdout);
-usleep(30000000);   
- }
+    {
+        strcat(mc_buffer, "$GPGSV,3,1,11,09,76,148,32,05,55,242,29,17,33,054,30,14,27,314,24*71");         
+        usleep(30000000);   
+    }
 }
 
 //thread to act as a data server that connects to the microcontroller and receives data from the same
 void *data_server(void *dservp){
-  char key;
-printf("Server to receive data from microcontroller");
+char key;
   //run a function every 31 seconds
   while(1){
-  printf("Connected to microcontroller...do you want to disconnect? ");
-  fflush(stdout);
-  scanf("%c",&key);
-  usleep(31000000);
+   if(connect){
+      //We need to flush the buffer if connected
+        printf("Data received: %s \n", mc_buffer);
+        mc_buffer[0] = '\0';
+        fflush(stdout);
+   }
+printf("Enter key ..C - connect/ D - disconnect\n");
+scanf("%c",&key);
+while((getchar())!='\n');
+fflush(stdout);
+//Logic to connect and disconnect from microcontroller
+if(key == 'C'){
+     connect = TRUE;
+     printf("connected...\n");
+     fflush(stdout);
+  }else if(key == 'D'){
+    connect = FALSE;
+    printf("Disconnected...\n");
+    fflush(stdout); 
+ }else{
+    printf("Invalid input...\n");
+}
+printf("waiting for data to be generated every 30 seconds...\n");
+//logic to process input ends
+ usleep(31000000);
   }
 }
 
